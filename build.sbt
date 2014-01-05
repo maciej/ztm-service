a6e5e9389b66031eb7bef01f3bc2c091991892ff
Maciej Bilas <maciej.bilas@gmail.com>
2014-01-05 20:24:16 +0100

    Add configuration.

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "ZTM service"
 
-version := "0.1"
+version := "0.1.1"
 
 scalaVersion := "2.10.3"
 
@@ -8,5 +8,7 @@ libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "2.0" % "test",
   "com.typesafe.slick" %% "slick" % "2.0.0-RC1",
   "org.slf4j" % "slf4j-nop" % "1.6.4",
-  "mysql" % "mysql-connector-java" % "5.1.28"
+  "mysql" % "mysql-connector-java" % "5.1.28",
+  "com.typesafe" % "config" % "1.0.2",
+  "com.escalatesoft.subcut" %% "subcut" % "2.0"
 )
---
+++ src/main/scala/pl/kodujdlapolski/ztm/config/CoreConfig.scala
@@ -0,0 +1,17 @@
+package pl.kodujdlapolski.ztm.config
+
+import com.typesafe.config.ConfigFactory
+
+class CoreConfig {
+  import CoreConfig._
+
+  def rootConfig = ConfigFactory.load().withFallback(ConfigFactory.load("pl/kodujdlapolski/ztm/defaults.conf"))
+
+  lazy val ztmDbHost = rootConfig.getString(s"$ZtmMysql.host")
+  lazy val ztmDbUsername = rootConfig.getString(s"$ZtmMysql.username")
+  lazy val ztmDbPassword = rootConfig.getString(s"$ZtmMysql.password")
+}
+
+object CoreConfig {
+  private val ZtmMysql = "ztm-mysql"
+}
