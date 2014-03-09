6075430bd653fa9edaaf49c2a2f60a90a6319318
Maciej Bilas <maciej.bilas@gmail.com>
2014-03-09 14:07:47 +0100

    Add remaining Metro Stations.

--- src/main/scala/pl/kodujdlapolski/ztm/records/Stations.scala
+++ src/main/scala/pl/kodujdlapolski/ztm/records/Stations.scala
@@ -35,6 +35,22 @@ object Stations extends Enumeration {
   val Natolin = Value(2, "Natolin")
   val Imielin = Value(3, "Imielin")
   val Stoklosy = Value(4, "Stoklosy")
+  val Ursynow = Value(5, "Ursynów")
+  val Sluzew = Value(6, "Służew")
+  val Wilanowska = Value(7, "Wilanowska")
+  val Wierzbno = Value(8, "Wierzbno")
+  val Raclawicka = Value(9, "Racławicka")
+  val PoleMokotowskie = Value(10, "Pole Mokotowskie")
+  val Politechnika = Value(11, "Politechnika")
+  val Centrum = Value(13, "Centrum")
+  val Swietokrzyska = Value(14, "Świetokrzyska")
+  val RatuszArsenal = Value(15, "Ratusz Arsenał")
+  val DworzecGdanski = Value(17, "Dworzec Gdański")
+  val PlacWilsona = Value(18, "Plac Wilsona")
+  val Marymont = Value(19, "Marymont")
+  val Slodowiec = Value(20, "Słodowiec")
+  val StareBielany = Value(21, "Stare Bielany")
+  val Wawrzysze = Value(22, "Wawrzyszew")
+  val Mlociny = Value(23, "Młociny")
 
-  /* I'm too lazy, add others yourself */
 }
