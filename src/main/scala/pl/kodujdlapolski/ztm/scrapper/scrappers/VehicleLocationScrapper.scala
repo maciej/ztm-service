f77b054424e351713d1e34d9fc4e0101bb4a0815
Maciej Bilas <maciej.bilas@gmail.com>
2014-03-16 15:37:29 +0100

    Scrapper.

--- src/main/scala/pl/kodujdlapolski/ztm/scrapper/scrappers/VehicleLocationScrapper.scala
+++ src/main/scala/pl/kodujdlapolski/ztm/scrapper/scrappers/VehicleLocationScrapper.scala
@@ -3,18 +3,24 @@ package pl.kodujdlapolski.ztm.scrapper.scrappers
 import akka.actor.Actor
 import pl.kodujdlapolski.ztm.records.VehicleLocationsProcProxy
 import pl.kodujdlapolski.ztm.scrapper.{ScheduleProvider, ScrapCommand}
+import pl.kodujdlapolski.ztm.mongodb.VehicleLocationRecord
+import com.typesafe.scalalogging.slf4j.Logging
 
 
-class VehicleLocationScrapper(proxy : VehicleLocationsProcProxy) extends Actor {
+class VehicleLocationScrapper(proxy: VehicleLocationsProcProxy) extends Actor with Logging {
 
   override def receive = {
-    case ScrapCommand =>
-      proxy.latestValidData()
-
+    case ScrapCommand => {
+      val beforeUpdateCount = VehicleLocationRecord.count
+      proxy.latestValidData().foreach(VehicleLocationRecord.addUpdate)
+      val afterUpdateCount = VehicleLocationRecord.count
+      logger.debug(s"Vehicle Location Scrapper. Updated ${afterUpdateCount - beforeUpdateCount} records.")
+    }
   }
 }
 
 object VehicleLocationScrapper extends ScheduleProvider {
+
   import scala.concurrent.duration._
 
   override val scheduleDelay = 10.seconds
