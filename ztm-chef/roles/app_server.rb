aa6bf6adca6a6c24dc186c64d3f7f4bbe8248a66
Maciej Bilas <maciej.bilas@gmail.com>
2014-03-16 17:11:31 +0100

    Chef: added mongodb provisioning.

--- ztm-chef/roles/app_server.rb
+++ ztm-chef/roles/app_server.rb
@@ -2,17 +2,22 @@ name 'app_server'
 description 'Application server'
 
 run_list [
-  'role[base]',
-  'recipe[java]'
-]
+             'role[base]',
+             'recipe[java]',
+             'recipe[mongodb::10gen_repo]',
+             'recipe[mongodb]'
+         ]
 
-default_attributes({
-  java: {
-    accept_license_agreement:   true,
-    oracle: {
-      accept_oracle_download_terms: true
+default_attributes(
+    java: {
+        accept_license_agreement: true,
+        oracle: {
+            accept_oracle_download_terms: true
+        },
+        install_flavor: 'oracle',
+        jdk_version: '7'
     },
-    install_flavor: 'oracle',
-    jdk_version: '7'
-  }
-})
+    mongodb: {
+        package_version: '2.4.9'
+    }
+)
